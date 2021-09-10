6a9e40475bc5063b46a5fa9f9485591f93629f58
Angel Estrada <aestrada@unitedvirtualities.com>
2021-09-10 14:15:14 -0600

    post user

--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ const users = [];
 app.get('/',(req, res) => {
     res.send('Hello World!')
 })
+
 app.get('/users',(req, res) => {
     res.send(users)
+})
+
+app.post('/users', (req, res) =>{
+    res.status(200).send(users[0]);
+    users.push({id:0});
 })
 
 app.listen(3000,function(){
