e07fccedb99ebe55f36a3f5a5e70bb035e1c2a72
Angel Estrada <aestrada@unitedvirtualities.com>
2021-09-13 09:55:02 -0600

    adding delete

--- src/index.js
+++ src/index.js
@@ -20,6 +20,11 @@ app.get('/user/:id', (req, res) =>{
     res.send({ id: 0 });
 });
 
+app.delete('user/:id', (req, res) =>{
+    users[0] ? res.status(202).send(users) : res.status(204).send();
+    users.pop();
+})
+
 app.listen(3000,function(){
     console.log("server is running");
     
